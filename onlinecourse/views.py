aa8ce0fec24800d205ff3167ead6903f9679d632
s224055534 <ppngodidi322@gmail.com>
2026-09-01 07:31:53 -0400

    last

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -139,32 +139,27 @@ def submit(request, course_id):
     choices = extract_answers(request)
     submission.choices.set(choices)
     submission_id = submission.id
+    
     return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
 
+def show_exam_result(request, course_id, submission_id):
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
 
-	def show_exam_result(request, course_id, submission_id):
-		context = {}
-		course = get_object_or_404(Course, pk=course_id)
-		submission = Submission.objects.get(id=submission_id)
-		choices = submission.choices.all()
+    total_score = 0
+    questions = course.question_set.all()
 
-		total_score = 0
-		questions = course.question_set.all()  # Assuming course has related questions
+    for question in questions:
+        correct_choices = question.choice_set.filter(is_correct = True)
+        selected_choices = choices.filter(question=question)
 
-		for question in questions:
-			correct_choices = question.choice_set.filter(is_correct=True)  # Get all correct choices for the question
-			selected_choices = choices.filter(question=question)  # Get the user's selected choices for the question
+        if set(correct_choices) == set(selected_choices):
+            total_score += question.grade
+    
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
 
-			# Check if the selected choices are the same as the correct choices
-			if set(correct_choices) == set(selected_choices):
-				total_score += question.grade  # Add the question's grade only if all correct answers are selected
-
-		context['course'] = course
-		context['grade'] = total_score
-		context['choices'] = choices
-
-		return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
-
-
-
-
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
